1522526fa2434dcfdc4976f71124c7fa0654f6a8
vishakh <vishakh@farefirst.com>
2024-08-20 12:21:38 +0530

    Add info.plist to gitignore

---
+++ git_pratical2/data manager.swift
@@ -0,0 +1,14 @@
+//
+//  data manager.swift
+//  git_pratical2
+//
+//  Created by Vishakh on 20/08/24.
+//
+
+import Foundation
+
+class DataManager{
+    func printMessage(){
+        print("print Meassage")
+    }
+}
